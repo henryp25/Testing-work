e89c7aca6808d5cd715e9bc866c21a056653bb51
Henry <hprice@merkleinc.com>
2022-03-24 17:50:17 +0000

    final run

--- file.js
+++ file.js
@@ -3,27 +3,28 @@ let myLibrary = [];
 function Book(book, user) {
   this.book = book
   this.user = user
-
 }
-
-let userBook = document.getElementById('bname').innerText
-let userName = document.getElementById('uname').innerText
-const userInput = new Book(userBook, userName)
-
-userBook.addEventListener('submit', (event) => {
-  const para = document.createElement("p");
-  const node = document.createTextNode(userBook)
-  para.appendChild(node)
-  let element = document.getElementById('div2');
-  element.appendChild(para)
-})
+let smit = document.getElementById("mybutton");
 
 function addBookToLibrary(){
+  let userBook = document.getElementById('bname').value
+  let userName = document.getElementById('uname').value
+  const userInput = new Book(userBook, userName)
   for(var key in userInput) {
     myLibrary.push(userInput[key])
   }
   console.log(myLibrary)
+  console.log(`Hello ${userName} you fave book is ${userBook}`)
+  const para1 = document.createElement("p");
+  const para2 = document.createElement("p");
+  para1.innerHTML = `Book:\n${userInput.book}`
+  para2.innerHTML = `Name:\n${userInput.user}`
+  let element = document.getElementById('div2');
+  element.appendChild(para1)
+  element.appendChild(para2)
 
 }
 
-console.log(addBookToLibrary())
+smit.addEventListener('click', function() {
+  addBookToLibrary()
+})
